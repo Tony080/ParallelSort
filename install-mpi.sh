a6302ea15a98280a632dd85591f153ff36f0c9a3
Chengzhi Hu <tony.hu1213@gmail.com>
2018-06-10 00:38:45 -0700

    Update install-mpi.sh

--- install-mpi.sh
+++ install-mpi.sh
@@ -4,4 +4,5 @@ wget --no-check-certificate https://download.open-mpi.org/release/open-mpi/v3.1/
 tar -xjf openmpi-3.1.0.tar.bz2
 cd openmpi-3.1.0
 ./configure --prefix=$HOME/local/OpenMPI3.1.0 && make -j 4 && make install && make clean
+PATH = $HOME/local/OpenMPI3.1.0/bin;$PATH
 cd ../../
